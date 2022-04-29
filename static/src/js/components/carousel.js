6d55b8db0f392568632cf70ed75b078562117591
Daine Mawer <hello@dainemawer.com>
2022-04-29 14:39:22 +0200

    tweaks to product page

---
+++ static/src/js/components/carousel.js
@@ -0,0 +1,13 @@
+const Carousel = () => {
+  console.log('Carousel')
+  const carousel = document.getElementById('product-carousel')
+  if (carousel) {
+    return new Flickity(carousel, {
+      cellAlign: 'center',
+      resize: true,
+      prevNextButtons: false
+    })
+  }
+}
+
+Carousel()
