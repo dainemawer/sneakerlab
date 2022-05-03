ce8e836db5cecb8fc6cdc241c9c8dd450773ed27
Daine Mawer <hello@dainemawer.com>
2022-05-03 17:02:50 +0200

    a11y updates to modal

--- static/src/js/components/modal.js
+++ static/src/js/components/modal.js
@@ -4,16 +4,19 @@ const Modal = () => {
   const modal = document.querySelector('[data-modal]')
 
   triggers && triggers.forEach(trigger => {
-    console.log(trigger)
     trigger.addEventListener('click', (e) => {
       e.preventDefault()
 
       if (!modal.classList.contains('modal-active')) {
         modal.classList.add('modal-active')
         document.body.style.overflow = 'hidden'
+        e.target.setAttribute('aria-expanded', 'true')
+        modal.setAttribute('aria-hidden', 'false')
       } else {
         modal.classList.remove('modal-active')
         document.body.style.overflow = 'auto'
+        e.target.setAttribute('aria-expanded', 'false')
+        modal.setAttribute('aria-hidden', 'true')
       }
     })
 
@@ -21,21 +24,27 @@ const Modal = () => {
       if (!modal.contains(e.target) && e.target !== trigger) {
         modal.classList.remove('modal-active')
         document.body.style.overflow = 'auto'
+        trigger.setAttribute('aria-expanded', 'false')
+        modal.setAttribute('aria-hidden', 'true')
       }
     })
-  })
 
-  close && close.addEventListener('click', (e) => {
-    e.preventDefault()
-    modal.classList.remove('modal-active')
-    document.body.style.overflow = 'auto'
-  })
-
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') {
+    close && close.addEventListener('click', (e) => {
+      e.preventDefault()
       modal.classList.remove('modal-active')
       document.body.style.overflow = 'auto'
-    }
+      trigger.setAttribute('aria-expanded', 'false')
+      modal.setAttribute('aria-hidden', 'true')
+    })
+
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        modal.classList.remove('modal-active')
+        document.body.style.overflow = 'auto'
+        trigger.setAttribute('aria-expanded', 'false')
+        modal.setAttribute('aria-hidden', 'true')
+      }
+    })
   })
 }
 
