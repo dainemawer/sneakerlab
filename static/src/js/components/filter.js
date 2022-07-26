f9f0765f72a2753c8aa1c30c6b5f83753bdfc7f2
Luke <luke@bermont.digital>
2022-07-26 10:49:39 +0200

    some local fixes

--- static/src/js/components/filter.js
+++ static/src/js/components/filter.js
@@ -5,11 +5,13 @@ const Filter = () => {
   trigger && trigger.addEventListener('click', () => {
     if (!panel.classList.contains('filter-active')) {
       trigger.innerHTML = 'Filter-'
+      trigger.classList.add('focus:opacity-50')
       trigger.setAttribute('aria-expanded', 'true')
       panel.setAttribute('aria-hidden', 'false')
       panel.classList.add('filter-active')
     } else {
       trigger.innerHTML = 'Filter+'
+      trigger.classList.remove('focus:opacity-50')
       trigger.setAttribute('aria-expanded', 'false')
       panel.setAttribute('aria-hidden', 'true')
       panel.classList.remove('filter-active')
